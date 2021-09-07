75a6124d92e12a513c7d2b4890b2446820fdb8e8
Matthew <kohney319m@gmail.com>
2021-09-07 17:51:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+
+  function clickAlert() {
+    alert('Yooooooo!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
